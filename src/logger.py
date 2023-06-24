2aba0fe4dbdb96712518171160cfb294108d7a59
mauro-balades <mauro.balades@tutanota.com>
2023-06-24 09:25:32 +0200

    rewrote logger to python

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+from colorama import Fore, Style
+
+class Logger:
+    """This utility class is used to log messages to the console."""
+
+    prefix = {
+        'info': Fore.CYAN + Style.BRIGHT + "info"
+    }
+    
+    @staticmethod
+    def info(msg):
+        print("[{}]: {}".format(Logger.prefix['info'], msg))
